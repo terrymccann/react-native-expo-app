97005990cdf66bccf5b72b425eb34b61cda893c8
TheCodingCoach <TheCoadingCoach@Terrys-MBP-2.home>
2023-03-25 13:59:16 -0400

    initial commit

---
+++ app/index.js
@@ -0,0 +1,15 @@
+import { useState } from 'react';
+import { View, ScrollView, SafeAreaView } from 'react-native';
+import { Stack, useRouter } from 'expo-router';
+
+import { COLORS, icons, images, SIZES } from '../constants';
+
+const Home = () => {
+    return (
+        <View>
+            <Text>Home</Text>
+        </View>
+    )
+}
+
+export default Home;
